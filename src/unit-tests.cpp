2dfcc6ad5c8874819c3a5461bf1c77613f37bdaf
Bruce MacKinnon <bruce@mackinnon.com>
2025-10-20 13:06:57 -0400

    Working

--- src/unit-tests.cpp
+++ src/unit-tests.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 using namespace kc1fsz;
 
+/*
+These test cases are drawn from the official standards document. See:
+
+https://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-G.711-198811-I!!PDF-E&type=items
+
+Tables 2a and 2b are relevant.
+
+The <<2 is needed because the test cases are all stated in terms of 14-bit PCM 
+values whereas the functions assume 16-bit PCM values.
+*/
 int main(int,const char**) {
 
     assert(encode_ulaw(-8159 << 2) == 0b00000000);
